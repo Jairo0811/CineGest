ac583c0c2461a243bf5936d4dd01a710d48b064e
Jairo Matías <jairo_1829@hotmail.com>
2026-08-17 13:55:20 -0400

    feat: conectar dashboard y reportes

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("apps.dashboard.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("apps.accounts.urls")),
+    path("reportes/", include("apps.reportes.urls")),
 ]
