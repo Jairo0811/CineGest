f78fbb1afddb31386dd6c39a4673fd24a7aa32cc
Jairo Matías <jairo_1829@hotmail.com>
2026-08-17 19:00:51 -0400

    feat(ui): habilitar rutas de gestión nativa

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", include("apps.dashboard.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("apps.accounts.urls")),
+    path("gestion/", include("apps.gestion.urls")),
     path("rentas/", include("apps.rentas.urls")),
     path("reportes/", include("apps.reportes.urls")),
 ]
